248e21ee58a1da5f021f959386e915576af8413c
Rohit Akole <akole.rohit50@gmail.com>
2024-07-16 14:03:53 -0400

    Updating Second Page

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,97 +22,97 @@
 		};
 
 	// Breakpoints.
-		breakpoints({
-			xlarge:  [ '1281px',  '1800px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ '481px',   '736px'  ],
-			xsmall:  [ null,      '480px'  ],
-		});
+	breakpoints({
+		xlarge:  [ '1281px',  '1800px' ],
+		large:   [ '981px',   '1280px' ],
+		medium:  [ '737px',   '980px'  ],
+		small:   [ '481px',   '736px'  ],
+		xsmall:  [ null,      '480px'  ],
+	});
 
 	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+	$window.on('load', function() {
+		window.setTimeout(function() {
+			$body.removeClass('is-preload');
+		}, 100);
+	});
 
 	// Touch?
-		if (browser.mobile) {
+	if (browser.mobile) {
 
-			// Turn on touch mode.
-				$body.addClass('is-touch');
+		// Turn on touch mode.
+			$body.addClass('is-touch');
 
-			// Height fix (mostly for iOS).
-				window.setTimeout(function() {
-					$window.scrollTop($window.scrollTop() + 1);
-				}, 0);
+		// Height fix (mostly for iOS).
+			window.setTimeout(function() {
+				$window.scrollTop($window.scrollTop() + 1);
+			}, 0);
 
-		}
+	}
 
 	// Footer.
-		breakpoints.on('<=medium', function() {
-			$footer.insertAfter($main);
-		});
+	breakpoints.on('<=medium', function() {
+		$footer.insertAfter($main);
+	});
 
-		breakpoints.on('>medium', function() {
-			$footer.appendTo($header);
-		});
+	breakpoints.on('>medium', function() {
+		$footer.appendTo($header);
+	});
 
 	// Header.
 
-		// Parallax background.
+	// Parallax background.
 
-			// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
-				if (browser.name == 'ie'
-				||	browser.mobile)
-					settings.parallax = false;
+		// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
+			if (browser.name == 'ie'
+			||	browser.mobile)
+				settings.parallax = false;
 
-			if (settings.parallax) {
+		if (settings.parallax) {
 
-				breakpoints.on('<=medium', function() {
+			breakpoints.on('<=medium', function() {
 
-					$window.off('scroll.strata_parallax');
-					$header.css('background-position', '');
+				$window.off('scroll.strata_parallax');
+				$header.css('background-position', '');
 
-				});
+			});
 
-				breakpoints.on('>medium', function() {
+			breakpoints.on('>medium', function() {
 
-					$header.css('background-position', 'left 0px');
+				$header.css('background-position', 'left 0px');
 
-					$window.on('scroll.strata_parallax', function() {
-						$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
-					});
-
-				});
-
-				$window.on('load', function() {
-					$window.triggerHandler('scroll');
-				});
-
-			}
-
-	// Main Sections: Two.
-
-		// Lightbox gallery.
-			$window.on('load', function() {
-
-				$('#two').poptrox({
-					caption: function($a) { return $a.next('h3').text(); },
-					overlayColor: '#2c2c2c',
-					overlayOpacity: 0.85,
-					popupCloserText: '',
-					popupLoaderText: '',
-					selector: '.work-item a.image',
-					usePopupCaption: true,
-					usePopupDefaultStyling: false,
-					usePopupEasyClose: false,
-					usePopupNav: true,
-					windowMargin: (breakpoints.active('<=small') ? 0 : 50)
+				$window.on('scroll.strata_parallax', function() {
+					$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
 				});
 
 			});
+
+			$window.on('load', function() {
+				$window.triggerHandler('scroll');
+			});
+
+		}
+
+	// Main Sections: Two.
+
+	// Lightbox gallery.
+		$window.on('load', function() {
+
+			$('#two').poptrox({
+				caption: function($a) { return $a.next('h3').text(); },
+				overlayColor: '#2c2c2c',
+				overlayOpacity: 0.85,
+				popupCloserText: '',
+				popupLoaderText: '',
+				selector: '.work-item a.image',
+				usePopupCaption: true,
+				usePopupDefaultStyling: false,
+				usePopupEasyClose: false,
+				usePopupNav: true,
+				windowMargin: (breakpoints.active('<=small') ? 0 : 50)
+			});
+
+		});
 	// Tab Functionality
 		window.openTab = function(tabName) {
 			// Hide all tab contents
@@ -121,8 +121,8 @@
 				content.classList.remove('active');
 			});
 
-			// Remove active class from all tab buttons
-			const tabButtons = document.querySelectorAll('.tab-link');
+			// Remove active class from all tab links
+			const tabLinks = document.querySelectorAll('.tab-link');
 			tabLinks.forEach(link => {
 				link.classList.remove('active');
 			});
@@ -130,6 +130,6 @@
 			// Show the selected tab content and add active class to the corresponding button
 			document.getElementById(tabName).classList.add('active');
 			document.querySelector(`[onclick="openTab('${tabName}')"]`).classList.add('active');
-		};
+	};
 
 })(jQuery);
