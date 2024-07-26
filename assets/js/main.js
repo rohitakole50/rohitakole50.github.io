48e5840b57ca6056169554d06ed6ea9c2f49534d
Rohit Akole <akole.rohit50@gmail.com>
2024-07-26 14:13:45 -0400

    Fixing alignment

--- assets/js/main.js
+++ assets/js/main.js
@@ -244,20 +244,36 @@
 		}
 	}
 
-	// Show the button when the user scrolls down 20px from the top of the document
+	// Back to Top
 	window.onscroll = function() {
-		var backToTopButton = document.getElementById("back-to-top");
-		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-			backToTopButton.style.display = "block";
-		} else {
-			backToTopButton.style.display = "none";
-		}
-	};
+        myFunction();
+        showBackToTopButton();
+    };
+
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+
+    function myFunction() {
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky");
+        } else {
+            navbar.classList.remove("sticky");
+        }
+    }
+
+    function showBackToTopButton() {
+        var backToTopButton = document.getElementById("back-to-top");
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            backToTopButton.style.display = "block";
+        } else {
+            backToTopButton.style.display = "none";
+        }
+    }
 
 })(jQuery);
 
 // Function to scroll to the top of the document
-	function scrollToTop() {
-		document.body.scrollTop = 0; // For Safari
-		document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
-	}
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
